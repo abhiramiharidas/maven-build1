2e2dfacd2170f3b24cdf8f09bf34f28544f25154
abhiramiharidas <amiiihari08@gmail.com>
2026-03-05 16:05:16 +0530

    app.java

--- app.java
+++ app.java
@@ -1,7 +1,5 @@
-package com.example;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven Project!");
+        System.out.println("Hello from Jenkins CI Pipeline");
     }
 }
