d7fd3d59acf4021b4fe04ef84d103b925d24c09c
abhiramiharidas <amiiihari08@gmail.com>
2026-03-06 16:32:21 +0530

    Create apptest.java

---
+++ apptest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        int a = 5;
+        int b = 5;
+        assertEquals(a, b);
+    }
+}
